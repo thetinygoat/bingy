ffdf16eae4e557b50d59ba0dee400a6443ec1832
Sachin Saini <iamsainisachin@gmail.com>
2019-03-18 19:14:31 +0530

    fix long title probelm

--- src/components/MovieSection/MovieScroller/MovieScroller.js
+++ src/components/MovieSection/MovieScroller/MovieScroller.js
@@ -3,24 +3,37 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const Poster = styled.img`
-	width: 100px;
 	margin-right: 10px;
 	border-radius: 10px;
+	@media (max-width: 730px) {
+		width: 120px;
+	}
 `;
 const Scroller = styled.section`
 	display: flex;
+`;
+const Title = styled.p`
+	text-decoration: none;
+	color: #e2e2e2;
+	font-size: .8em;
+	margin-top: .2em
+	font-weight: bold;
+	display: flex
+	flex-wrap:wrap
+	width: 90%
 `;
 const MovieScroller = props => {
 	let data = props.category.content[0].map(movie => {
 		return (
 			<Link key={movie.unique_id} to={`/content/${movie.unique_id}`}>
-				<Poster
-					src={movie.poster}
-					alt={movie.title}
-					key={movie.poster}
-					height="142.16px"
-					width="100px"
-				/>
+				<Poster src={movie.poster} alt={movie.title} key={movie.poster} />
+				<Title>
+					{/* {movie.title.length > 10
+						? movie.title.substring(0, 10) + '...'
+						: movie.title}
+					({movie.original_release_year}) */}
+					{movie.title}({movie.original_release_year})
+				</Title>
 			</Link>
 		);
 	});
